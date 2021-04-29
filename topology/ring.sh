ac5f2ff963a61d51c79b903f836a5b1d8d22dc8d
David Rodrigues De Almeida <david.rodrigues-de-almeida@etu.utc.fr>
2021-04-29 21:38:26 +0200

    Maj avec une topologie qui fonctionne, anneau unidirectionnel 1 -> 2 -> 3

--- topology/ring.sh
+++ topology/ring.sh
@@ -1,4 +1,10 @@
-rm -f in* out*
+# Faire le ménage
+killall bas.py
+killall net.py
+killall cat
+killall tee
+
+# Crée les pipes
 mkfifo /tmp/in1a /tmp/out1a
 mkfifo /tmp/in1b /tmp/out1b
 mkfifo /tmp/in2a /tmp/out2a
@@ -6,15 +12,18 @@ mkfifo /tmp/in2b /tmp/out2b
 mkfifo /tmp/in3a /tmp/out3a
 mkfifo /tmp/in3b /tmp/out3b
 
-./bas.py --auto --ident=node1 --whatwho --begin=CLT --dest=NET < /tmp/in1a > /tmp/out1a &
-./net.py --auto --ident=node1 --whatwho --begin=BAS < /tmp/in1b > /tmp/out1b &
 
-./bas.py --auto --ident=node2 --whatwho --begin=GCH --dest=NET < /tmp/in2a > /tmp/out2a &
-./net.py --auto --ident=node2 --whatwho --begin=BAS < /tmp/in2b > /tmp/out2b &
+# Crée les apps et redirection de stdin et out de chacune
+./bas.py --auto --ident=node1 --whatwho --bas-dest=NET < /tmp/in1a > /tmp/out1a &
+./net.py --auto --ident=node1 --whatwho < /tmp/in1b > /tmp/out1b &
 
-./bas.py --auto --ident=node3 --whatwho --begin=CLT --dest=NET < /tmp/in3a > /tmp/out3a &
-./net.py --auto --ident=node3 --whatwho --begin=BAS < /tmp/in3b > /tmp/out3b &
+./bas.py --auto --ident=node2 --whatwho --bas-dest=NET < /tmp/in2a > /tmp/out2a &
+./net.py --auto --ident=node2 --whatwho  < /tmp/in2b > /tmp/out2b &
 
+./bas.py --auto --ident=node3 --whatwho --begin=NET --bas-dest=NET < /tmp/in3a > /tmp/out3a &
+./net.py --auto --ident=node3 --whatwho < /tmp/in3b > /tmp/out3b &
+
+# redirection des pipes
 cat /tmp/out1a > /tmp/in1b &
 cat /tmp/out1b | tee /tmp/in1a > /tmp/in2b &
 cat /tmp/out2a > /tmp/in2b &
