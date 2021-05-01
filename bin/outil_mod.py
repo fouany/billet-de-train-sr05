062a200824d1d09562d035a181f36d90ff170789
Candice Bimont <candice.bimont@etu.utc.fr>
2021-05-01 17:09:48 +0200

    ajout de getNode et modification de GetPayloade en getInstance

--- bin/outil_mod.py
+++ bin/outil_mod.py
@@ -1,10 +1,12 @@
 
-def GetPayload(msg):
-    n=ligne.find('payload')
-    msg=msg[n+8:]
+def GetInstance(msg):
+    return GetNode(msg,'instance')
+
+def GetNode(msg,type):
+    n=msg.find(type)
+    msg=msg[n+len(type)+1:]
     m=msg.find('^')
     msg=msg[:m]
     return msg
-
-# ligne="^appnet~CTL^clientDemendeur~NET^clientTransmetteur~Net^Imp~5^nseq~5^payload~Hello Word^type~requete^typeDemande~consultation^"
-# print (GetPayload(ligne))
+# ligne="^appnet~CTL^clientDemendeur~NET^clientTransmetteur~Net^Imp~5^nseq~5^instance~Hello Word^type~requete^typeDemande~consultation^"
+# print (GetNode(ligne,'clientTransmetteur'))
